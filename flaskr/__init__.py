dd4da1069896944b1c62f21a83ab703847258259
Craig Lamont <biglam86@gmail.com>
2020-10-07 15:07:53 +0100

    PEP 8indentation

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,143 +4,143 @@ from flask_sqlalchemy import SQLAlchemy
 
 from models import setup_db, Actor, Movie
 
+
 def create_app(test_config=None):
-  app = Flask(__name__)
-  setup_db(app)
+    app = Flask(__name__)
+    setup_db(app)
 
+    # Actor endpoints
+    @app.route('/actors')
+    def get_all_actors():
+        actors = Actor.query.all()
 
-# Actor endpoints
-  @app.route('/actors')
-  def get_all_actors():
-    actors = Actor.query.all()
+        return jsonify({
+            'success': True,
+            'actors': [actor.format() for actor in actors]
+        })
 
-    return jsonify({
-      'success': True,
-      'actors':  [actor.format() for actor in actors]
-    })
+    @app.route('/actors/<int:id>')
+    def get_actor(id):
+        actor = Actor.query.get(id)
+        # TODO: check if found
+        return jsonify({
+            'success': True,
+            'actor': actor.format()
+        })
 
-  @app.route('/actors/<int:id>')
-  def get_actor(id):
-    actor = Actor.query.get(id)
-    #TODO: check if found
-    return jsonify({
-      'success': True,
-      'actor': actor.format()
-    })
+    @app.route('/actors/', methods=['POST'])
+    def create_actor():
+        form = request.get_json()
+        # TODO: validate form
+        actor = Actor(form.get('name'), form.get('age'), form.get('gender'))
+        actor.insert()
+        # TODO: Check if inserted
+        return jsonify({
+            'success': True,
+            'actor': actor.format()
+        }), 200
 
-  @app.route('/actors/', methods=['POST'])
-  def create_actor():
-    form = request.get_json()
-    #TODO: validate form
-    actor = Actor(form.get('name'), form.get('age'), form.get('gender'))
-    actor.insert()
-    #TODO: Check if inserted
-    return jsonify({
-      'success': True,
-      'actor': actor.format()
-    }), 200
+    @app.route('/actors/<int:id>', methods=['PATCH'])
+    def update_actor(id):
+        form = request.get_json()
+        # TODO: Validate form
+        actor = Actor.query.get(id)
+        # TODO: check actor exists
 
-  @app.route('/actors/<int:id>', methods=['PATCH'])
-  def update_actor(id):
-    form = request.get_json()
-    # TODO: Validate form
-    actor = Actor.query.get(id)
-    # TODO: check actor exists
+        if form.get('name'):
+            actor.name = form.get('name')
 
-    if form.get('name'):
-      actor.name = form.get('name')
+        if form.get('age'):
+            actor.age = form.get('age')
 
-    if form.get('age'):
-      actor.age = form.get('age')
+        if form.get('gender'):
+            actor.gender = form.get('gender')
 
-    if form.get('gender'):
-      actor.gender = form.get('gender')
+        actor.update()
+        # TODO: Check successfully updated
+        return jsonify({
+            'success': True,
+            'actor': actor.format()
+        })
 
-    actor.update()
-    # TODO: Check successfully updated
-    return jsonify({
-      'success': True,
-      'actor': actor.format()
-    })
+    @app.route('/actors/<int:id>', methods=['DELETE'])
+    def delete_actor(id):
+        actor = Actor.query.get(id)
+        # TODO: check exists
 
-  @app.route('/actors/<int:id>', methods=['DELETE'])
-  def delete_actor(id):
-    actor = Actor.query.get(id)
-    # TODO: check exists
+        actor.delete()
+        # TODO: check deleted
 
-    actor.delete()
-    # TODO: check deleted
+        return jsonify({
+            'success': True,
+            'deleted_id': actor.id
+        })
 
-    return jsonify({
-      'success': True,
-      'deleted_id': actor.id
-    })
+    # Movie endpoints
+    @app.route('/movies')
+    def get_all_movies():
+        movies = Movie.query.all()
 
-  # Movie endpoints
-  @app.route('/movies')
-  def get_all_movies():
-    movies = Movie.query.all()
+        return jsonify({
+            'success': True,
+            'movies': [movie.format() for movie in movies]
+        })
 
-    return jsonify({
-      'success': True,
-      'movies': [movie.format() for movie in movies]
-    })
+    @app.route('/movies/<int:id>')
+    def get_movie(id):
+        movie = Movie.query.get(id)
+        # TODO: check if found
+        return jsonify({
+            'success': True,
+            'movie': movie.format()
+        })
 
-  @app.route('/movies/<int:id>')
-  def get_movie(id):
-    movie = Movie.query.get(id)
-    # TODO: check if found
-    return jsonify({
-      'success': True,
-      'movie': movie.format()
-    })
+    @app.route('/movies/', methods=['POST'])
+    def create_movie():
+        form = request.get_json()
+        # TODO: validate form
+        release_date = datetime.strptime(form.get('release_date'), '%Y-%m-%d')
+        print(release_date)
+        movie = Movie(form.get('title'), release_date)
+        movie.insert()
+        # TODO: Check if inserted
+        return jsonify({
+            'success': True,
+            'movie': movie.format()
+        }), 200
 
-  @app.route('/movies/', methods=['POST'])
-  def create_movie():
-    form = request.get_json()
-    # TODO: validate form
-    release_date = datetime.strptime(form.get('release_date'), '%Y-%m-%d')
-    print(release_date)
-    movie = Movie(form.get('title'), release_date)
-    movie.insert()
-    # TODO: Check if inserted
-    return jsonify({
-      'success': True,
-      'movie': movie.format()
-    }), 200
+    @app.route('/movies/<int:id>', methods=['PATCH'])
+    def update_movie(id):
+        form = request.get_json()
+        # TODO: Validate form
+        movie = Movie.query.get(id)
+        # TODO: check movie exists
 
-  @app.route('/movies/<int:id>', methods=['PATCH'])
-  def update_movie(id):
-    form = request.get_json()
-    # TODO: Validate form
-    movie = Movie.query.get(id)
-    # TODO: check movie exists
+        if form.get('title'):
+            movie.title = form.get('title')
 
-    if form.get('title'):
-      movie.title = form.get('title')
+        if form.get('release_date'):
+            release_date = datetime.strptime(form.get('release_date'), '%Y-%m-%d')
+            movie.release_date = release_date
 
-    if form.get('release_date'):
-      release_date = datetime.strptime(form.get('release_date'), '%Y-%m-%d')
-      movie.release_date = release_date
+        movie.update()
+        # TODO: Check successfully updated
+        return jsonify({
+            'success': True,
+            'movie': movie.format()
+        })
 
-    movie.update()
-    # TODO: Check successfully updated
-    return jsonify({
-      'success': True,
-      'movie': movie.format()
-    })
+    @app.route('/movies/<int:id>', methods=['DELETE'])
+    def delete_movie(id):
+        movie = Movie.query.get(id)
+        # TODO: check exists
 
-  @app.route('/movies/<int:id>', methods=['DELETE'])
-  def delete_movie(id):
-    movie = Movie.query.get(id)
-    # TODO: check exists
+        movie.delete()
+        # TODO: check deleted
 
-    movie.delete()
-    # TODO: check deleted
+        return jsonify({
+            'success': True,
+            'deleted_id': movie.id
+        })
 
-    return jsonify({
-      'success': True,
-      'deleted_id': movie.id
-    })
-
-  return app
+    return app
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import os
 from sqlalchemy import Column, String, Integer, Date, create_engine
 from flask_sqlalchemy import SQLAlchemy
+
 # from flask_migrate import Migrate
 
 postgres_username = os.environ.get('POSTGRES_USERNAME')
@@ -11,73 +12,78 @@ database_path = "postgres://{}:{}@{}/{}".format(postgres_username, postgres_pass
 
 db = SQLAlchemy()
 
+
 def setup_db(app, database_path=database_path):
-  app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-  app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-  db.app = app
-  db.init_app(app)
-  db.create_all()
-  # migrate = Migrate(app, db)
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+    # migrate = Migrate(app, db)
+
 
 class Actor(db.Model):
-  __tablename__ = 'actors'
+    __tablename__ = 'actors'
 
-  id = Column(Integer, primary_key=True)
-  name = Column(String, nullable=False)
-  age = Column(Integer, nullable=False)
-  gender = Column(String, nullable=False) #TODO: regex
-  # TODO: movies
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
+    gender = Column(String, nullable=False)  # TODO: regex
 
-  def __init__(self, name, age, gender):
-    self.name = name
-    self.age = age
-    self.gender = gender
-  
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
-  
-  def update(self):
-    db.session.commit()
+    # TODO: movies
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'age': self.age,
-      'gender': self.gender
-    }
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender
+        }
+
 
 class Movie(db.Model):
-  __tablename__ = 'movies'
+    __tablename__ = 'movies'
 
-  id = db.Column(Integer, primary_key=True)
-  title = db.Column(String, unique=True)
-  release_date = db.Column(Date(), nullable=False)
-  #TODO: Actors
+    id = db.Column(Integer, primary_key=True)
+    title = db.Column(String, unique=True)
+    release_date = db.Column(Date(), nullable=False)
 
-  def insert(self):
-    db.session.add(self)
-    db.session.commit()
+    # TODO: Actors
 
-  def update(self):
-    db.session.commit()
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
 
-  def delete(self):
-    db.session.delete(self)
-    db.session.commit()
+    def update(self):
+        db.session.commit()
 
-  def __init__(self, title, release_date):
-    self.title = title
-    self.release_date = release_date
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
-  def format(self):
-    return {
-      'id': self.id,
-      'title': self.title,
-      'release_date': self.release_date
-    }
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date
+        }
