866372581b742173e47a40d73569c5cbfc307a7f
Craig Lamont <biglam86@gmail.com>
2020-10-10 23:10:19 +0100

    removoing unused vars

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 export FLASK_APP=app
 export FLASK_ENV=development
 export DATABASE_URL="postgres://USER:PASSWORD@localhost:5432/capstone"
-export AUTH0_DOMAIN="udac-capstone.eu.auth0.com"
-export AUTH0_CLIENT_ID="9PBdGhtlOfPeSVAZQxFTWTils3RgbWdb"
-export AUTH0_SECRET="_nxUOaGNyZYbOnYfqt8xSjNlCE1nUJYbqRGtBG9_jP150Xdp73tZLCY08Cr2lcL7"
 
 flask run
