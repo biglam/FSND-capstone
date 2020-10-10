ac48a9b340d04616fe7c99fa1382775e35e2a6a5
Craig Lamont <biglam86@gmail.com>
2020-10-10 22:35:14 +0100

    correcting setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 export FLASK_APP=app
 export FLASK_ENV=development
-export DATABASE_URL="postgres://lam:lam585@localhost:5432/capstone"
+export DATABASE_URL="postgres://USER:PASSWORD@localhost:5432/capstone"
 export AUTH0_DOMAIN="udac-capstone.eu.auth0.com"
 export AUTH0_CLIENT_ID="9PBdGhtlOfPeSVAZQxFTWTils3RgbWdb"
 export AUTH0_SECRET="_nxUOaGNyZYbOnYfqt8xSjNlCE1nUJYbqRGtBG9_jP150Xdp73tZLCY08Cr2lcL7"
