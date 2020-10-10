3b8460f38ee38d8a5046d0a1b4fef5366cdda55e
Craig Lamont <biglam86@gmail.com>
2020-10-10 22:36:18 +0100

    correcting setup

--- setup.sh
+++ setup.sh
@@ -5,5 +5,4 @@ export AUTH0_DOMAIN="udac-capstone.eu.auth0.com"
 export AUTH0_CLIENT_ID="9PBdGhtlOfPeSVAZQxFTWTils3RgbWdb"
 export AUTH0_SECRET="_nxUOaGNyZYbOnYfqt8xSjNlCE1nUJYbqRGtBG9_jP150Xdp73tZLCY08Cr2lcL7"
 
-sh ./postgres_env.sh
 flask run
